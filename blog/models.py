6c46223f336f868eaad6559a1be2db5054c1cf30
Jacob-Yearoo <jacob.yearoo@hotmail.com>
2023-01-11 00:03:01 +0000

    build models for comment and post inputs

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.Post.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80, unique=True)
     email = models.EmailField(unique=True)
     body = models.TextField()
