43d186ed4ee944612955a030195d4bf3bc0381cd
Jacob-Yearoo <jacob.yearoo@hotmail.com>
2023-01-11 17:41:05 +0000

    link URLs to templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from djanog.views import generic
+from django.views import generic
 from .models import Post
 
 
-class PostList(generic.listview):
+class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
